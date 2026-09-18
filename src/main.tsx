fe544e4783ebf9e725c26426f540f063658c4172
cowhezzbiz <biz.cowhezz@proton.me>
2026-09-18 16:32:46 -0300

    BrowserRouter para URL limpa

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 import AdminPage from './AdminPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/barber-morais-admin" element={<AdminPage />} />
